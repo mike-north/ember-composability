af0fbc2ec521dcd88dc42eb76c2e9adacc1d5e26
Mike North <michael.l.north@gmail.com>
2015-08-16 10:26:42 -0700

    raise on deprecation

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,6 +1,10 @@
+import Ember from 'ember';
 import resolver from './helpers/resolver';
+
 import {
   setResolver
 } from 'ember-qunit';
+
+Ember.ENV.RAISE_ON_DEPRECATION = true;
 
 setResolver(resolver);
