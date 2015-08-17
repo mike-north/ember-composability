e588891c7dfdd36207508296a77068036c2b14ac
Mike North <michael.l.north@gmail.com>
2015-08-16 17:25:03 -0700

    fix broken build

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -31,7 +31,7 @@ module.exports = function(environment) {
     // Testem prefers this...
     ENV.baseURL = '/';
     ENV.locationType = 'none';
-    ENV.EmberENV.RAISE_ON_DEPRECATION: !process.env['ALLOW_DEPRECATIONS'];
+    ENV.EmberENV.RAISE_ON_DEPRECATION = !process.env['ALLOW_DEPRECATIONS'];
     // keep test console output quieter
     ENV.APP.LOG_ACTIVE_GENERATION = false;
     ENV.APP.LOG_VIEW_LOOKUPS = false;
